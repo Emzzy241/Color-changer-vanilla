18042790edf1c504a11ea9742f5704faf516721e
Emzzy241 <emzzyoluwole@gmail.com>
2022-08-14 15:14:35 +0100

    writing the script.js file for changing colors

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Creating variables for storing the button, and the body tags
+// Note that: document.getElementByClassName() wouldn't work
+// only document.queerySelector() works
+
+
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+
+const color = ["red", "pink", "violet", "blue"];
+
+body.style.backgroundColor = "pink";
+
