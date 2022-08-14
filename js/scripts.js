f4915722683e2e699dacd9a70f82980770f3e9dd
Emzzy241 <emzzyoluwole@gmail.com>
2022-08-14 15:57:36 +0100

    writing the function for the third task

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,20 @@
 const button = document.querySelector("button");
 const body = document.querySelector("body");
 
+// This array was used for only the third task 
 const color = ["red", "pink", "violet", "blue", "green", "brown", "#fff", "purple"];
 
 body.style.backgroundColor = "pink";
 
+// This array was used for only the second task
+
+const color2Task = ["red", "grey", "blue", "green", "white", "black"]
+
 
 button.addEventListener("click", functionChanger);
 
-/* the function for the first color changer, changing background-Color to red
+/* the first task 
+the function for the first color changer, changing background-Color to red
 
 function functionChanger(){
     const redCol = "red";
@@ -24,7 +30,23 @@ function functionChanger(){
 
 */
 
-/*
+/* the second task function which is a function for creating
+many buttons, when user clicks on any button, it changes to that specific color
+*/
+
+function functionChanger(){
+    const redBg = "red";
+    const greyBg = "grey";
+    const blueBg = "blue";
+    const greenBg = "green";
+    const whiteBg = "white";
+    const darkBg = "black"
+}
+
+
+
+/* the function for the third task,  create an array of colors
+randomize those colors whenever we click on a button
 function functionChanger() {
     const colorRed = parseInt(Math.random() * color.length);
     body.style.backgroundColor = color[colorRed];
